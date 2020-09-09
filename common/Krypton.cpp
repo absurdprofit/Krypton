16b3e5d4bddf911c7ce64b12f4d3e61946cb5967
Nathan Johnson <nahtanjohn1l@gmail.com>
2020-09-08 19:12:01 -0500

    Added DrawElement

--- common/Krypton.cpp
+++ common/Krypton.cpp
@@ -96,30 +96,49 @@ Krypton::Krypton(ContextData *ContextData)
             _round_indices[j + 0] = centre;
             _round_indices[j + 1] = offset;
             _round_indices[j + 2] = ++offset;
-            count+=3;
+            count += 3;
         }
         centre = ++offset;
         offset++;
     }
 }
 
-void Krypton::DrawCorner(float cx, float cy, float r, float offset)
+void Krypton::DrawCorner(float cx, float cy, float r, OFFSET offset)
 {
     Vertex c;
     c.position = {cx, cy, 0.0f};
-    c.colour = {1.0f, 0.0f, 0.0f, 1.0f};
+    c.colour = {0.0f, 1.0f, 0.0f, 1.0f};
 
     _round_vertices[numRoundVertices] = c;
     //populate vertices
+    float tau;
+    switch (offset)
+    {
+    case TOP_LEFT_CORNER:
+        tau = 3.1415926f;
+        break;
+
+    case TOP_RIGHT_CORNER:
+        tau = 3 * 3.1415926f / 2.0f;
+        break;
+
+    case BOTTOM_LEFT_CORNER:
+        tau = 3.1415926f / 2.0f;
+        break;
+
+    case BOTTOM_RIGHT_CORNER:
+        tau = 0.0f;
+        break;
+    }
     for (int i = 0; i < numRoundSegments; i++)
     {
         float theta = (3.1415926f / 2.0f) * (float)i / (float)numRoundSegments;
-        float x = r * cosf(offset + theta);
-        float y = r * sinf(offset + theta);
+        float x = r * cosf(tau + theta);
+        float y = r * sinf(tau + theta);
         //https://lh3.googleusercontent.com/proxy/_v_q758R5xyNSdwLzvUBaCm1I2k5nCXGYW1tVfGbCcLqJGxrvZWDjpWJsXP92CX9VfMT4_KWUcRVC1xCe6kZ_NyBQw
         Vertex v;
         v.position = {cx + x, cy + y, 0.0f};
-        v.colour = {1.0f, 0.0f, 0.0f, 1.0f};
+        v.colour = {0.0f, 1.0f, 0.0f, 1.0f};
         _round_vertices[numRoundVertices + i + 1] = v;
     }
     //number of indices
@@ -128,20 +147,20 @@ void Krypton::DrawCorner(float cx, float cy, float r, float offset)
 }
 
 //create a green quad with position (x, y), height h and width w
-void Krypton::CreateQuad(vec3f position, float w, float h)
+void Krypton::CreateQuad(Vertex v1, Vertex v2, Vertex v3, Vertex v4)
 {
-    Vertex v1;
-    v1.position = {position.x - w, position.y + h, position.z};
-    v1.colour = {0.0f, 1.0f, 0.0f, 1.0f};
-    Vertex v2;
-    v2.position = {position.x + w, position.y + h, position.z};
-    v2.colour = {0.0f, 1.0f, 0.0f, 1.0f};
-    Vertex v3;
-    v3.position = {position.x - w, position.y - h, position.z};
-    v3.colour = {0.0f, 1.0f, 0.0f, 1.0f};
-    Vertex v4;
-    v4.position = {position.x + w, position.y - h, position.z};
-    v4.colour = {0.0f, 1.0f, 0.0f, 1.0f};
+    // Vertex v1;
+    // v1.position = {position.x - w, position.y + h, position.z};
+    // v1.colour = {0.0f, 1.0f, 0.0f, 1.0f};
+    // Vertex v2;
+    // v2.position = {position.x + w, position.y + h, position.z};
+    // v2.colour = {0.0f, 1.0f, 0.0f, 1.0f};
+    // Vertex v3;
+    // v3.position = {position.x - w, position.y - h, position.z};
+    // v3.colour = {0.0f, 1.0f, 0.0f, 1.0f};
+    // Vertex v4;
+    // v4.position = {position.x + w, position.y - h, position.z};
+    // v4.colour = {0.0f, 1.0f, 0.0f, 1.0f};
 
     _quad_vertices[numQuadVertices] = v1;
     _quad_vertices[numQuadVertices + 1] = v2;
@@ -154,6 +173,48 @@ void Krypton::CreateQuad(vec3f position, float w, float h)
 
 void Krypton::DrawElement(Element element)
 {
+    // vec3f position = {_contextData->width / 2.0f, _contextData->height / 2.0f};
+    // CreateQuad(position, 50, 50);
+    // vec3f position3 = {_contextData->width / 2.0f + 50.0f, _contextData->height / 2.0f};
+    // CreateQuad(position3, 50, 50);
+    // vec3f position4 = {_contextData->width / 2.0f, _contextData->height / 2.0f + 50.0f};
+    // CreateQuad(position4, 50, 50);
+    // vec3f position5 = {_contextData->width / 2.0f, _contextData->height / 2.0f - 50.0f};
+    // CreateQuad(position5, 50, 50);
+    // vec3f position6 = {_contextData->width / 2.0f - 50.0f, _contextData->height / 2.0f};
+    // CreateQuad(position6, 50, 50);
+
+    // DrawCorner(_contextData->width / 2.0f + 50.0f, _contextData->height / 2.0f + 50.0f, 50.0f, 0.0f);
+    // DrawCorner(_contextData->width / 2.0f - 50.0f, _contextData->height / 2.0f + 50.0f, 50.0f, 3.1415926f / 2.0f);
+    // DrawCorner(_contextData->width / 2.0f + 50.0f, _contextData->height / 2.0f - 50.0f, 50.0f, 3 * 3.1415926f / 2.0f);
+    // DrawCorner(_contextData->width / 2.0f - 50.0f, _contextData->height / 2.0f - 50.0f, 50.0f, 3.1415926f);
+
+    vec4f colour = {0.0f, 1.0f, 0.0f, 1.0f};
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Vertex v4;
+    //top left
+    v1.position = {element.position.x - element.width, element.position.y - element.height, element.position.z};
+    v1.colour = colour;
+    //top right
+    v2.position = {element.position.x + element.width, element.position.y - element.height, element.position.z};
+    v2.colour = colour;
+    //bottom left
+    v3.position = {element.position.x - element.width, element.position.y + element.height, element.position.z};
+    v3.colour = colour;
+    //bottom right
+    v4.position = {element.position.x + element.width, element.position.y + element.height, element.position.z};
+    v4.colour = colour;
+    CreateQuad(v1, v2, v3, v4);
+    //top left
+    DrawCorner(element.position.x - element.width, element.position.y - element.height, element.borderTopLeftRadius, TOP_LEFT_CORNER);
+    //top right
+    DrawCorner(element.position.x + element.width, element.position.y - element.height, element.borderTopRightRadius, TOP_RIGHT_CORNER);
+    //bottom left
+    DrawCorner(element.position.x - element.width, element.position.y + element.height, element.borderBottomLeftRadius, BOTTOM_LEFT_CORNER);
+    //bottom right
+    DrawCorner(element.position.x + element.width, element.position.y + element.height, element.borderBottomRightRadius, BOTTOM_RIGHT_CORNER);
 }
 
 std::function<void()> loop;
@@ -166,7 +227,6 @@ void Krypton::Draw()
 {
 
     running = true;
-
 
     loop = [&] {
         if (!running)
@@ -214,26 +274,12 @@ void Krypton::Update()
     numRoundIndices = 0;
     numRoundVertices = 0;
 
-    vec3f position2 = {0.0f, 0.0f};
-    CreateQuad(position2, 50, 50);
-
-    vec3f position = {_contextData->width / 2.0f, _contextData->height / 2.0f};
-    CreateQuad(position, 50, 50);
-    vec3f position3 = {_contextData->width / 2.0f + 50.0f, _contextData->height / 2.0f};
-    CreateQuad(position3, 50, 50);
-    vec3f position4 = {_contextData->width / 2.0f, _contextData->height / 2.0f + 50.0f};
-    CreateQuad(position4, 50, 50);
-    vec3f position5 = {_contextData->width / 2.0f, _contextData->height / 2.0f - 50.0f};
-    CreateQuad(position5, 50, 50);
-    vec3f position6 = {_contextData->width / 2.0f - 50.0f, _contextData->height / 2.0f};
-    CreateQuad(position6, 50, 50);
-
-    DrawCorner(_contextData->width / 2.0f + 50.0f, _contextData->height / 2.0f + 50.0f, 50.0f, 0.0f);
-    DrawCorner(_contextData->width / 2.0f - 50.0f, _contextData->height / 2.0f + 50.0f, 50.0f, 3.1415926f / 2.0f);
-    DrawCorner(_contextData->width / 2.0f + 50.0f, _contextData->height / 2.0f - 50.0f, 50.0f, 3 * 3.1415926f / 2.0f);
-    DrawCorner(_contextData->width / 2.0f - 50.0f, _contextData->height / 2.0f - 50.0f, 50.0f, 3.1415926f);
-
-    
+    Element square(_contextData->width / 2.0f, _contextData->height / 2.0f, 0.0f, 50.0f, 50.0f);
+    square.borderTopLeftRadius = 50.0f;
+    square.borderTopRightRadius = 20.0f;
+    square.borderBottomRightRadius = 30.0f;
+    square.borderBottomLeftRadius = 10.0f;
+    DrawElement(square);
 
     glUseProgram(*_contextData->programObjects);
 }
--- common/Krypton.h
+++ common/Krypton.h
@@ -2,6 +2,13 @@
 #include "KryptonMath.h"
 #include <vector>
 
+enum OFFSET {
+    TOP_LEFT_CORNER,
+    TOP_RIGHT_CORNER,
+    BOTTOM_LEFT_CORNER,
+    BOTTOM_RIGHT_CORNER
+};
+
 struct Element {
     Vertex vert;
     vec3f position;
@@ -11,6 +18,16 @@ struct Element {
     float borderTopLeftRadius = 0.0f;
     float borderBottomRightRadius = 0.0f;
     float borderBottomLeftRadius = 0.0f;
+
+    Element(float x=0.0f, float y=0.0f, float z=0.0f, float w=0.0f, float h=0.0f) {
+        position.x = x;
+        position.y = y;
+        position.z = z;
+        width = w;
+        height = h;
+        vert.position = position;
+        vert.colour = { 1.0f, 0.0f, 0.0f, 1.0f };
+    }
 };
 
 class Krypton {
@@ -19,9 +36,9 @@ public:
     void Draw();
     void Update();
     void Clean();
-    void DrawCorner(float, float, float, float);
+    void DrawCorner(float, float, float, OFFSET);
     void DrawElement(Element);
-    void CreateQuad(vec3f, float, float);
+    void CreateQuad(Vertex, Vertex, Vertex, Vertex);
 
 private:
     ContextData* _contextData;
